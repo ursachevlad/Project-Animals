7cd028b38cd1ef520c6568c8656ec8d59e29bba7
Vlad <ursachevladmihai@yahoo.com>
2019-06-25 22:28:29 +0300

    First commit on my project

---
+++ Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAnimals
+{
+    class Animal
+    {
+        protected string name;
+        protected int age;
+        protected string food;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Food
+        {
+            get { return food; }
+            set { food = value; }
+        }
+
+        public void Description()
+        {
+            Console.WriteLine(name + "has" + age + "years and eats" + food);
+        }
+            
+    }
+}
